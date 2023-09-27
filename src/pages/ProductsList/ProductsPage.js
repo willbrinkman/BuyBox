399ec1a2d750eef24cb113af9c0906f7922294c0
Will Brinkman <willbkman@gmail.com>
2023-09-26 22:19:01 -0500

    implemented data fetching of products

--- src/pages/ProductsList/ProductsPage.js
+++ src/pages/ProductsList/ProductsPage.js
@@ -6,11 +6,24 @@ const ProductsPage = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-
+    useEffect(() => {
+        async function loadProducts() {
+          try {
+            const data = await fetchAllProducts();
+            setProducts(data);
+          } catch (err) {
+            setError(err.message);
+          } finally {
+            setLoading(false);
+          }
+        }
+    
+        loadProducts();
+      }, []);
     return (
 <div>
     <h1>Products</h1>
-    
+
 </div>
     );
 };
