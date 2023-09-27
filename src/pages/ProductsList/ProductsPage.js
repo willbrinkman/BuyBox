abb74e7d181a78a979d49a73090453e4885ea398
Will Brinkman <willbkman@gmail.com>
2023-09-26 22:20:59 -0500

    implemented error handling and loading indicator

--- src/pages/ProductsList/ProductsPage.js
+++ src/pages/ProductsList/ProductsPage.js
@@ -1,31 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import { fetchAllProducts } from '../services/api';
+import React, { useState, useEffect } from "react";
+import { fetchAllProducts } from "../services/api";
 
 const ProductsPage = () => {
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        async function loadProducts() {
-          try {
-            const data = await fetchAllProducts();
-            setProducts(data);
-          } catch (err) {
-            setError(err.message);
-          } finally {
-            setLoading(false);
-          }
-        }
-    
-        loadProducts();
-      }, []);
-    return (
-<div>
-    <h1>Products</h1>
+  useEffect(() => {
+    async function loadProducts() {
+      try {
+        const data = await fetchAllProducts();
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
 
-</div>
-    );
+    loadProducts();
+  }, []);
+
+  if (error) return <p>Error: {error}</p>;
+  if (loading) return <p>Loading...</p>;
+  if (!products.length) return <p>No products found.</p>;
+
+  return (
+    <div>
+      <h1>Products</h1>
+    </div>
+  );
 };
 
 export default ProductsPage;
