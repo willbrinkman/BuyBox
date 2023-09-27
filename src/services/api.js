bb57dae6691d4007cb2a3648ff0f8feef5a5adbe
Will Brinkman <willbkman@gmail.com>
2023-09-26 20:56:11 -0500

    implemented loginUser API call

--- src/services/api.js
+++ src/services/api.js
@@ -25,7 +25,7 @@ export const fetchAllProducts = async () => {
       const data = await handleResponse(response);
       return data;
     } catch (error) {
-      console.error("Error fetching product with ID: ${id}:", error);
+      console.error(`Error fetching product with ID: ${id}:`, error);
       throw error;
     }
   };
@@ -40,3 +40,40 @@ export const fetchAllProducts = async () => {
       throw error;
     }
   };
+
+  export const loginUser = async (username, password) => {
+    try {
+      const response = await fetch(`${BASE_URL}/auth/login`, {
+        method: "POST",
+        body: JSON.stringify({
+          user: {
+            username,
+            password,
+          },
+        }),
+      });
+      return handleResponse(response);
+    } catch (error) {
+      console.error(`Error logging in user: ${username}:`, error);
+      throw error;
+    }
+  };
+
+  export const loginUser = async (username, password) => {
+    try {
+      const response = await fetch(`${BASE_URL}/auth/login`, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username,
+          password
+        }),
+      });
+      return handleResponse(response);
+    } catch (error) {
+      console.error(`Error logging in user: ${username}:`, error);
+      throw error;
+    }
+};
