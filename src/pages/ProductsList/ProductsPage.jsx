90ce2856efa57ae50a4003753dfef50340b6330b
Will Brinkman <willbkman@gmail.com>
2023-09-26 22:46:09 -0500

    Feature/products list (#4)
    * initialized core API setup and basic error-handling function
    
    * implemeted fetchAllProducts API call
    
    * implemented fetchSingleProduct API call
    
    * implemented fetchAllCategories API call
    
    * implemented loginUser API call
    
    * implemented createCart API call
    
    * standardized JSON headers and refactored code
    
    * initialized core structure for list products page
    
    * implemented data fetching of products
    
    * implemented error handling and loading indicator
    
    * Implemented product list rendering and basic styling

---
+++ src/pages/ProductsList/ProductsPage.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import { fetchAllProducts } from "../../services/api.js";
+
+const ProductsPage = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function loadProducts() {
+      try {
+        const data = await fetchAllProducts();
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    loadProducts();
+  }, []);
+
+  if (error) return <p>Error: {error}</p>;
+  if (loading) return <p>Loading...</p>;
+  if (!products.length) return <p>No products found.</p>;
+
+  return (
+    <div>
+      <h1>Products</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <img
+              src={product.image}
+              alt={product.title}
+              className="product-image"
+            />
+            <h3>{product.title}</h3>
+            <p>Price: ${product.price}</p>{" "}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductsPage;
