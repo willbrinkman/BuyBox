aa17174437dc4e9fa2cc6998da978a988c8b78ac
Will Brinkman <willbkman@gmail.com>
2023-09-26 21:43:14 -0500

    Feature/api integration (#3)
    * initialized core API setup and basic error-handling function
    
    * implemeted fetchAllProducts API call
    
    * implemented fetchSingleProduct API call
    
    * implemented fetchAllCategories API call
    
    * implemented loginUser API call
    
    * implemented createCart API call
    
    * standardized JSON headers and refactored code

---
+++ src/services/api.js
@@ -0,0 +1,82 @@
+const BASE_URL = "https://fakestoreapi.com/";
+
+const JSON_HEADERS = {
+    'Content-Type': 'application/json'
+};
+
+const handleResponse = async (response) => {
+  if (!response.ok) {
+    throw new Error("Server error");
+  }
+  return response.json();
+};
+
+export const fetchAllProducts = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/products`);
+    const data = await handleResponse(response);
+    return data;
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    throw error;
+  }
+};
+
+export const fetchSingleProduct = async (id) => {
+  try {
+    const response = await fetch(`${BASE_URL}/products/${id}`);
+    const data = await handleResponse(response);
+    return data;
+  } catch (error) {
+    console.error(`Error fetching product with ID: ${id}:`, error);
+    throw error;
+  }
+};
+
+export const fetchAllCategories = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/products/categories`);
+    const data = await handleResponse(response);
+    return data;
+  } catch (error) {
+    console.error("Error fetching product categories", error);
+    throw error;
+  }
+};
+
+export const loginUser = async (username, password) => {
+  try {
+    const response = await fetch(`${BASE_URL}/auth/login`, {
+      method: "POST",
+      headers: JSON_HEADERS,
+      body: JSON.stringify({
+        user: {
+          username,
+          password,
+        },
+      }),
+    });
+    return handleResponse(response);
+  } catch (error) {
+    console.error(`Error logging in user: ${username}:`, error);
+    throw error;
+  }
+};
+
+export const createCart = async (userId, date, products) => {
+  try {
+    const response = await fetch(`${BASE_URL}/carts`, {
+      method: "POST",
+      headers: JSON_HEADERS,
+      body: JSON.stringify({
+          userId: userId,
+          date: date,
+          products: products,
+      }),
+    });
+    return handleResponse(response);
+  } catch (error) {
+    console.error(`Error creating cart:`, error);
+    throw error;
+  }
+};
