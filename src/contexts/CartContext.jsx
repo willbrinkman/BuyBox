e744bbf66d7b016264d7d055ac6442b8a9b85be3
Will Brinkman <willbkman@gmail.com>
2023-09-28 11:51:09 -0500

    Implemented session storage for session persistance of cart data

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,9 +1,20 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const cartContext = createContext();
 
 export const CartProvider = ({ children }) => {
     const [cart, setCart] = useState([]);
+
+    useEffect(() => {
+        const storedCart = sessionStorage.getItem('cart');
+        if (storedCart) {
+          setCart(JSON.parse(storedCart));
+        }
+      }, []);
+    
+      useEffect(() => {
+        sessionStorage.setItem('cart', JSON.stringify(cart));
+      }, [cart]);
 
 return (
     <cartContext.Provider>
