1b2b7b13c9692e8cde283ee21d4000aff8484324
Will Brinkman <willbkman@gmail.com>
2023-09-28 09:09:35 -0500

    implemented core login form structure

---
+++ src/pages/Login/LoginPage.js
@@ -0,0 +1,19 @@
+const LoginPage = () => {
+return (
+    <div>
+        <form>
+        <label>
+                <input></input>
+            </label>
+            <br />
+            <label>
+                <input></input>
+            </label>
+            <br />
+            <button></button>
+        </form>
+    </div>
+)
+};
+
+export default LoginPage;
