f02df5ceb1ed55218d71a0c9efad78873e184e90
Will Brinkman <willbkman@gmail.com>
2023-09-28 09:13:11 -0500

    completed form implementation

--- src/pages/Login/LoginPage.js
+++ src/pages/Login/LoginPage.js
@@ -1,19 +1,42 @@
 const LoginPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const { login } = useContext(AuthContext);
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        await login(username, password); 
+      } catch (error) {
+        console.error("Login failed", error);
+        alert("Failed to login");
+      }
+    };
 return (
     <div>
-        <form>
+      <form onSubmit={handleSubmit}>
         <label>
-                <input></input>
-            </label>
-            <br />
-            <label>
-                <input></input>
-            </label>
-            <br />
-            <button></button>
-        </form>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
     </div>
-)
+  );
 };
 
 export default LoginPage;
