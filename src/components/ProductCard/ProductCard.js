0ea7cce8df3833f7e8b5652cc4e6a16e8b6baf76
Will Brinkman <willbkman@gmail.com>
2023-09-28 00:07:59 -0500

    Feature/product card (#5)
    * Implemented basic structure for product card
    
    * Implemented product display from properties
    
    * implemented condtional diquantity adjustment, product add and remove, and condtional displays to productCard

---
+++ src/components/ProductCard/ProductCard.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+const ProductCard = ({
+  product,
+  showDetail,
+  onUpdateCart,
+  onRemove,
+  showRemove,
+}) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleIncrement = () => {
+    setQuantity((prevQuantity) => prevQuantity + 1);
+  };
+
+  const handleDecrement = () => {
+    setQuantity((prevQuantity) => Math.max(1, prevQuantity - 1));
+  };
+
+  const handleUpdateCart = () => {
+    onUpdateCart(product, quantity);
+  };
+
+  return (
+    <div>
+      <img src={product.image} alt={product.title} />
+      <h3>{product.title}</h3>
+      <p>Price: ${product.price}</p>
+      {showDetail && <p>{product.description}</p>}
+      {showDetail && ( // Changed from showAdjust to showDetail
+        <>
+          <div className="quantity-adjuster">
+            <button onClick={handleDecrement} disabled={quantity <= 1}>
+              -
+            </button>
+            <span>{quantity}</span>
+            <button onClick={handleIncrement}>+</button>
+          </div>
+          <button onClick={handleUpdateCart}>Update Cart</button>
+        </>
+      )}
+      {showRemove && <button onClick={() => onRemove(product)}>Remove</button>}
+    </div>
+  );
+};
+
+export default ProductCard;
