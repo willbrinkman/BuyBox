834ecd0dbafb6e0a16c4b468461dca59411cfd2e
Will Brinkman <willbkman@gmail.com>
2023-09-28 12:49:47 -0500

    Feature/sort filter (#9)
    * implemented category filter
    
    * implemented price range filter
    
    * implemented alphabetical and price sort
    
    * minor bug fixes

--- src/pages/ProductsList/ProductsPage.jsx
+++ src/pages/ProductsList/ProductsPage.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
-import { fetchAllProducts } from "../../services/api.js";
+import { fetchAllProducts, fetchAllCategories } from "../../services/api.js";
 
 const ProductsPage = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [priceRange, setPriceRange] = useState([0, Infinity]);
+  const [category, setCategory] = useState("");
+  const [sort, setSort] = useState("");
 
   useEffect(() => {
     async function loadProducts() {
@@ -21,6 +24,40 @@ const ProductsPage = () => {
     loadProducts();
   }, []);
 
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    async function fetchCategories() {
+      try {
+        const cat = await fetchAllCategories();
+        setCategories(cat);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    fetchCategories();
+  }, []);
+
+  const displayedProducts = products
+    .filter((product) => (category ? product.category === category : true))
+    .filter(
+      (product) =>
+        product.price >= priceRange[0] && product.price <= priceRange[1]
+    )
+    .sort((a, b) => {
+      switch (sort) {
+        case "asc":
+          return a.title.localeCompare(b.title);
+        case "desc":
+          return b.title.localeCompare(a.title);
+        case "priceLowHigh":
+          return a.price - b.price;
+        case "priceHighLow":
+          return b.price - a.price;
+        default:
+          return 0;
+      }
+    });
+
   if (error) return <p>Error: {error}</p>;
   if (loading) return <p>Loading...</p>;
   if (!products.length) return <p>No products found.</p>;
@@ -28,8 +65,47 @@ const ProductsPage = () => {
   return (
     <div>
       <h1>Products</h1>
+      <label>
+        Category:
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
+          <option value="">All Categories</option>
+          {categories.map((cat) => (
+            <option key={cat} value={cat}>
+              {cat}
+            </option>
+          ))}
+        </select>
+      </label>
+      <label>
+        Min: $
+        <input
+          type="number"
+          value={priceRange[0]}
+          onChange={(e) =>
+            setPriceRange([Number(e.target.value), priceRange[1]])
+          }
+        />
+        Max: $
+        <input
+          type="number"
+          value={priceRange[1]}
+          onChange={(e) =>
+            setPriceRange([priceRange[0], Number(e.target.value)])
+          }
+        />
+      </label>
+      <label>
+        Sort By:
+        <select value={sort} onChange={(e) => setSort(e.target.value)}>
+          <option value="">Select...</option>
+          <option value="asc">A-Z</option>
+          <option value="desc">Z-A</option>
+          <option value="priceLowHigh">Low to High</option>
+          <option value="priceHighLow">High to Low</option>
+        </select>
+      </label>
       <ul>
-        {products.map((product) => (
+        {displayedProducts.map((product) => (
           <li key={product.id}>
             <img
               src={product.image}
