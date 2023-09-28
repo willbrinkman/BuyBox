1f854a724faff64d5c5f98fd74c3df3efafac553
Will Brinkman <willbkman@gmail.com>
2023-09-27 23:31:20 -0500

    Implemented product display from properties

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,8 +1,11 @@
-const ProductCard = () => {
-    return (
-      <div>
-      </div>
-    );
-  };
+const ProductCard = ({product}) => {
+  return (
+    <div>
+      <img src={product.image} alt={product.title} />
+      <h3>{product.title}</h3>
+      <p>{product.price}</p>
+    </div>
+  );
+};
 
-  export default ProductCard;
+export default ProductCard;
