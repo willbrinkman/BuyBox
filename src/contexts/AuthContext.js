aaf2792ae624561b3172bb72b3ded8b4e7799370
Will Brinkman <willbkman@gmail.com>
2023-09-28 09:00:56 -0500

    set response token to context state

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,10 +1,29 @@
-import { createContext } from "react";
-
+import { createContext, useState } from "react";
+import { loginUser } from "../services/api";
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+    const [token, setToken] = useState(false);
 
-return (
-    <></>
-)
+    useEffect(() => {
+        const storedToken = localStorage.getItem('authToken');
+        if (storedToken) {
+          setToken(storedToken);
+        }
+    }, []);
+    const login = async (username, password) => {
+        try {
+          const response = await loginUser(username, password);
+          const userToken = response.token;
+          
+          localStorage.setItem('authToken', userToken);
+          setToken(userToken);
+        } catch (error) {
+          console.error("Failed to log in", error);
+          throw error;
+        }
+      };
+
+
+  return <AuthContext.Provider>{children}</AuthContext.Provider>;
 };
