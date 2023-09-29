6dfb462d2b1ad2a14d55114357bbcef329b983c5
Will Brinkman <willbkman@gmail.com>
2023-09-29 12:26:04 -0500

    styling and bug fixes

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -3,38 +3,39 @@ import { createContext, useState, useEffect } from "react";
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
 
-    const [cart, setCart] = useState([]);
+  useEffect(() => {
+    const storedCart = sessionStorage.getItem("cart");
+    if (storedCart) {
+      setCart(JSON.parse(storedCart));
+    }
+  }, []);
 
-    useEffect(() => {
-        const storedCart = sessionStorage.getItem('cart');
-        if (storedCart) {
-          setCart(JSON.parse(storedCart));
-        }
-      }, []);
-    
-      useEffect(() => {
-        sessionStorage.setItem('cart', JSON.stringify(cart));
-      }, [cart]);
+  useEffect(() => {
+    sessionStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
 
-      const addToCart = (product, quantity) => {
-        const existingProductIndex = cart.findIndex(item => item.product.id === product.id);
-    
-        if (existingProductIndex !== -1) {
-          const updatedCart = [...cart];
-          updatedCart[existingProductIndex].quantity += quantity;
-          setCart(updatedCart);
-        } else {
-          setCart(prevCart => [...prevCart, { product, quantity }]);
-        }
-      };
-    
-      const updateCart = (productId, newQuantity) => {
-        const updatedCart = cart.map(item => 
-          item.product.id === productId ? { ...item, quantity: newQuantity } : item
-        );
-        setCart(updatedCart);
-      };
+  const addToCart = (product, quantity) => {
+    const existingProductIndex = cart.findIndex(
+      (item) => item.product.id === product.id
+    );
+
+    if (existingProductIndex !== -1) {
+      const updatedCart = [...cart];
+      updatedCart[existingProductIndex].quantity += quantity;
+      setCart(updatedCart);
+    } else {
+      setCart((prevCart) => [...prevCart, { product, quantity }]);
+    }
+  };
+
+  const updateCart = (productId, newQuantity) => {
+    const updatedCart = cart.map((item) =>
+      item.product.id === productId ? { ...item, quantity: newQuantity } : item
+    );
+    setCart(updatedCart);
+  };
 
   const removeFromCart = (productId, action) => {
     const productIndex = cart.findIndex(
@@ -63,5 +64,5 @@ export const CartProvider = ({ children }) => {
     >
       {children}
     </CartContext.Provider>
-)
-
+  );
+};
